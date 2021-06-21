9e652c742a38408b5cda47148d4062443c867882
IiIymik <sambotemp@gmail.com>
2021-06-21 13:18:13 +0300

    add

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -4,6 +4,7 @@ import { showSpinner, hideSpinner} from './spiner.js';
 import Pagination from 'tui-pagination';
 import API from '../apiServises/apiService.js';
 
+creatDivEl("card__section", "tui-pagination");
 const options = {
   totalItems: 1000,
   itemsPerPage: 1,
@@ -14,8 +15,7 @@ const options = {
   lastItemClassName: 'tui-last-child',
 };
 
-const smoothScrool = function () {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+const smoothScrool = () =>  { window.scrollTo({ top: 0, behavior: 'smooth' });
 };
 
 const pagination = new Pagination('pagination', options);
@@ -32,10 +32,21 @@ function chooseRender(currentPage) {
 };
 
 pagination.on('afterMove', function (evt) {
+  let currentPage = evt.page;
   showSpinner();
   smoothScrool();
-     let currentPage = evt.page;
   chooseRender(currentPage);
 });
+
+function creatDivEl(selector, divSelector){
+  const elWhere = `.${selector}`;
+  const elWhich = `.${divSelector}`;
+  const findElWhere = document.querySelector(elWhere);
+
+  const createEL = `<div id="pagination" class="${elWhich}"></div>`
+  findElWhere.insertAdjacentHTML('afterend', createEL);
+};
+
+creatDivEl("card__section","tui-pagination");
 
 export {pagination};
